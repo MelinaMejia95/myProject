c93cb745a3753f38f920327beb99eff94e5267a6
MelinaMejia95 <melinamejia95@gmail.com>
2017-06-05 12:29:12 -0500

    First changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Little Monster';
+  author = 'Melina Mejía';
+  count = 0;
+  imgSource = './../favicon.ico';
+  enable = false;
 }
